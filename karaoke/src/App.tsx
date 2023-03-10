5f5c374f97eb8d9d82ade3d3bee85d4a2aab4e13
YYtech15 <midori1140flower@gmail.com>
2023-03-10 12:56:22 +0900

    FastAPIとの連携確認

--- karaoke/src/App.tsx
+++ karaoke/src/App.tsx
@@ -1,9 +1,28 @@
+import axios from "axios";
+import { useState } from "react";
 import './App.css'
 
+type Data = {
+  message: string;
+}
+
 const App = () => {
+  const [data, setData] = useState<Data>();
+  const url = "http://localhost:8000";
+
+  const GetData = () => {
+    axios.get(url)
+      .then((res) => {
+        setData(res.data);
+        console.log(res.data);
+      });
+  };
   return (
-    <div>App</div>
-  )
+    <div>
+      <div>ここに処理を書いていきます</div>
+      {data ? <div>{data.message}</div> : <button onClick={GetData}>データを取得</button>}
+    </div>
+  );
 }
 
 export default App
